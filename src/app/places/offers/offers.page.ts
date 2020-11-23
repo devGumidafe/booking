120d5ba2093fdd113eab1ddc6fb3000b0723ce4c
Gumidafe <devgumidafe@gmail.com>
2020-11-23 12:57:56 +0000

    Implements options-slide in offers.page.html

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -11,10 +13,15 @@ export class OffersPage implements OnInit {
 
   offers: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService,
+    private router: Router) { }
 
   ngOnInit() {
     this.offers = this.placesService.getAllPlaces();
   }
-
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'offers', 'edit', offerId]);
+    console.log('Edit item', offerId);
+  }
 }
