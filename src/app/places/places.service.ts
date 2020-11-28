c3b2336885db90f2e7fb17fd83b96dbb501d2651
Gumidafe <devgumidafe@gmail.com>
2020-11-28 10:35:07 +0000

    Configure the Date Controls

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -12,17 +12,26 @@ export class PlacesService {
       'Manhattan Mansion',
       'In the heart of New York City',
       'https://www.viajeselcorteingles.es/imagen/es/mic/nueva_york/home/img_mic_nueva_york_home_790x486_02.jpg',
-      149.99),
+      149.99,
+      new Date('2020-11-01'),
+      new Date('2021-12-31')
+    ),
     new Place('p2',
       'San Francisco',
       'In the heart of San Francisco City',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/San_Francisco_from_Twin_Peaks_September_2013_panorama_5_edit.jpg/1000px-San_Francisco_from_Twin_Peaks_September_2013_panorama_5_edit.jpg',
-      189.99),
+      189.99,
+      new Date('2020-11-01'),
+      new Date('2021-12-31')
+    ),
     new Place('p3',
       'Los Angeles',
       'In the heart of Los Angeles City',
       'https://www.visittheusa.mx/sites/default/files/styles/hero_l_x2/public/images/hero_media_image/2017-01/Getty_515070156_EDITORIALONLY_LosAngeles_HollywoodBlvd_Web72DPI_0.jpg?itok=z9ThSf2W',
-      99.99)
+      99.99,
+      new Date('2020-11-01'),
+      new Date('2021-12-31')
+    )
   ];
 
   constructor() { }
@@ -32,6 +41,6 @@ export class PlacesService {
   }
 
   getPlace(id: string) {
-    return {...this.places.find(p => p.id === id)};
+    return { ...this.places.find(p => p.id === id) };
   }
 }
